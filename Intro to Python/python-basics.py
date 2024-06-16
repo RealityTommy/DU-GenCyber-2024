edf3edd5e69130ab8db16423ccbc753542d0ecaf
Tommy Truong <ttruong@emergejobs.com>
2024-06-16 13:01:50 -0400

    Finished basics.

--- Intro to Python/python-basics.py
+++ Intro to Python/python-basics.py
@@ -8,7 +8,7 @@ comment.
 name = "Tommy"   # string data type
 age = 18         # integer data type
 health = 100.0   # float data type
-alive = True     # boolean data type
+hungry = True     # boolean data type
 inventory = ["sword", "shield", "cake"]  # list data type
 money = {"copper": 25, "silver": 2}   # dictionary data type
 quests = {"eat cake"}   # set data type
@@ -26,3 +26,29 @@ money["silver"] += 1    # arithmetic on item in dictionary
 
 quests.add("save kitten")   # add item to set
 quests.remove("eat cake")   # remove item from set
+
+print(welcome_message)              # display message in console
+name = input("What's your name? ")  # get user input
+
+# Condition with if else statement
+if health <= 0:
+    print("Player is passed out.")
+else:
+    print("Player is awake.")
+
+# Condition with elif
+if health <= 0:
+    print("Player is passed out.")
+elif health <= 50:
+    print("Player is weak.")
+else:
+    print("Player is awake.")
+
+# For loop to output inventory
+for item in inventory:
+    print(item)
+
+# While loop while player is fighting
+while health > 0:
+    health -= 10
+    print(health)
