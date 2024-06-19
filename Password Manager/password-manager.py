432c7b8eaef51355eafc4422f61eb3c1fdb01079
Tommy Truong <ttruong@emergejobs.com>
2024-06-18 21:40:56 -0400

    Added title.

--- Password Manager/password-manager.py
+++ Password Manager/password-manager.py
@@ -1,3 +1,7 @@
+'''
+Password Manager
+'''
+
 # secret formula to encrypt password by moving one letter over in the alphabet
 secretmap = {"a": "b", "b": "c", "c": "d", "d": "e", "e": "f", "f": "g", "g": "h", "h": "i", "i": "j", "j": "k", "k": "l", "l": "m", "m": "n", "n": "o", "o": "p", "p": "q", "q": "r", "r": "s", "s": "t", "t": "u", "u": "v", "v": "w", "w": "x", "x": "y", "y": "z", "z": "a"}
 
